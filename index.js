6871acf9346d37ef77fc5553800bdf9dd84da5d5
Austin  Burke <aburk3@gmail.com>
2018-09-01 21:48:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,7 +81,8 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return 'Sorry, we don\'t have a credit card on file for you.'
   } else {
-    let totalPrice = total()
+    //let totalPrice = total()
+    total()
     cart = []
     return `Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`
   }
