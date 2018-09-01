a0d2a427cb01e192fa575847511059f1679aa216
Austin  Burke <aburk3@gmail.com>
2018-09-01 21:43:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,6 +83,6 @@ function placeOrder(cardNumber) {
   } else {
     totalPrice = total()
     cart = []
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`
   }
 }
